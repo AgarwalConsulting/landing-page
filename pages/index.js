6ed2387735fd0e80f4ceae1485fa781e05035f98
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-10-18 00:17:20 +0530

    Landing page, init.

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+export default () => (
+  <div className="container">
+    <div className="row justify-content-md-center">
+      <div className="col col-lg-6">
+        Thank you for showing interest in SilkMon project!
+      </div>
+    </div>
+
+    <div className="row justify-content-md-center">
+      <div className="col col-lg-6">
+        The app is still being built, please check again later!
+      </div>
+    </div>
+
+    <div className="row justify-content-md-center">
+      <div className="col col-lg-6">
+        If you are curious, about <a href="https://blog.algogrit.com/posts/2018-10-04-starting-a-new-chapter/">my story, here is a glimpse</a> of it.
+      </div>
+    </div>
+  </div>
+)
